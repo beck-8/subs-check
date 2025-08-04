fengmas/gist1

feat: 添加ASN信息支持并启用媒体检测

check/check.go
@@ -35,6 +35,7 @@ type Result struct {
 	IP         string
 	IPRisk     string
 	Country    string
+	ASN        string
 }
 
 // ProxyChecker 处理代理检测的主要结构体
@@ -216,17 +217,26 @@ func (pc *ProxyChecker) checkProxy(proxy map[string]any) *Result {
 					res.Gemini = true
 				}
 			case "iprisk":
-				country, ip := proxyutils.GetProxyCountry(httpClient.Client)
-				if ip != "" && country != "" {
+				// 获取ip和国家信息
+				var ip string
+				res.Country, ip = proxyutils.GetProxyCountry(httpClient.Client)
+				if ip != "" {
 					res.IP = ip
-					res.Country = country
+					ipRisk, err := platfrom.CheckIPRisk(httpClient.Client, ip)
+					if err == nil && ipRisk != "" {
+						res.IPRisk = ipRisk
+					} else {
+						// 失败的可能性高，所以放上日志
+						slog.Debug(fmt.Sprintf("查询IP风险失败: %v", err))
+					}
 				}
-				risk, err := platfrom.CheckIPRisk(httpClient.Client, ip)
-				if err == nil {
-					res.IPRisk = risk
+			case "asn":
+				// 直接调用CheckASN
+				asn, err := platfrom.CheckASN(httpClient.Client)
+				if err == nil && asn != "" {
+					res.ASN = asn
 				} else {
-					// 失败的可能性高，所以放上日志
-					slog.Debug(fmt.Sprintf("查询IP风险失败: %v", err))
+					slog.Debug(fmt.Sprintf("查询ASN失败: %v", err))
 				}
 			}
 		}
@@ -262,8 +272,8 @@ func (pc *ProxyChecker) updateProxyName(res *Result, httpClient *ProxyClient, sp
 
 	name := res.Proxy["name"].(string)
 
-	// 移除所有已有的标记（包括速度、IPRisk和平台标记）
-	name = regexp.MustCompile(`\s*\|(?:Netflix|Disney|Youtube|Openai|Gemini|\d+%|\s*⬇️\s*[\d.]+[KM]B/s)`).ReplaceAllString(name, "")
+	// 移除所有已有的标记（包括速度、IPRisk、ASN和平台标记）
+	name = regexp.MustCompile(`\s*\|(?:Netflix|Disney|Youtube|Openai|Gemini|\d+%|AS\d+|\s*⬇️\s*[\d.]+[KM]B/s)`).ReplaceAllString(name, "")
 	name = strings.TrimSpace(name)
 
 	var tags []string
@@ -282,6 +292,12 @@ func (pc *ProxyChecker) updateProxyName(res *Result, httpClient *ProxyClient, sp
 	if res.IPRisk != "" {
 		tags = append(tags, res.IPRisk)
 	}
+
+	// 添加ASN信息
+	if res.ASN != "" {
+		tags = append(tags, "AS"+res.ASN)
+	}
+
 	if res.Netflix {
 		tags = append(tags, "Netflix")
 	}
@@ -304,7 +320,6 @@ func (pc *ProxyChecker) updateProxyName(res *Result, httpClient *ProxyClient, sp
 	}
 
 	res.Proxy["name"] = name
-
 }
 
 // showProgress 显示进度条

beck-8/subs-check

check/check.go
@@ -39,6 +39,7 @@ type Result struct {
 	IP         string
 	IPRisk     string
 	Country    string
+	ASN        string
 }
 
 // ProxyChecker 处理代理检测的主要结构体
@@ -245,18 +246,26 @@ func (pc *ProxyChecker) checkProxy(proxy map[string]any) *Result {
 					res.Gemini = true
 				}
 			case "iprisk":
-				country, ip := proxyutils.GetProxyCountry(httpClient.Client)
-				if ip == "" {
-					break
+				// 获取ip和国家信息
+				var ip string
+				res.Country, ip = proxyutils.GetProxyCountry(httpClient.Client)
+				if ip != "" {
+					res.IP = ip
+					ipRisk, err := platform.CheckIPRisk(httpClient.Client, ip)
+					if err == nil && ipRisk != "" {
+						res.IPRisk = ipRisk
+					} else {
+						// 失败的可能性高，所以放上日志
+						slog.Debug(fmt.Sprintf("查询IP风险失败: %v", err))
+					}
 				}
-				res.IP = ip
-				res.Country = country
-				risk, err := platform.CheckIPRisk(httpClient.Client, ip)
-				if err == nil {
-					res.IPRisk = risk
+			case "asn":
+				// 直接调用CheckASN
+				asn, err := platform.CheckASN(httpClient.Client)
+				if err == nil && asn != "" {
+					res.ASN = asn
 				} else {
-					// 失败的可能性高，所以放上日志
-					slog.Debug(fmt.Sprintf("查询IP风险失败: %v", err))
+					slog.Debug(fmt.Sprintf("查询ASN失败: %v", err))
 				}
 			case "tiktok":
 				if region, _ := platform.CheckTikTok(httpClient.Client); region != "" {
@@ -284,6 +293,9 @@ func (pc *ProxyChecker) updateProxyName(res *Result, httpClient *ProxyClient, sp
 	}
 
 	name := res.Proxy["name"].(string)
+
+	// 移除所有已有的标记（包括速度、IPRisk、ASN和平台标记）
+	name = regexp.MustCompile(`\s*\|(?:Netflix|Disney|Youtube|Openai|Gemini|\d+%|AS\d+|\s*⬇️\s*[\d.]+[KM]B/s)`).ReplaceAllString(name, "")
 	name = strings.TrimSpace(name)
 
 	var tags []string
@@ -329,6 +341,10 @@ func (pc *ProxyChecker) updateProxyName(res *Result, httpClient *ProxyClient, sp
 			if res.IPRisk != "" {
 				tags = append(tags, res.IPRisk)
 			}
+		case "asn":
+			if res.ASN != "" {
+				tags = append(tags, "AS"+res.ASN)
+			}
 		case "youtube":
 			if res.Youtube != "" {
 				tags = append(tags, fmt.Sprintf("YT-%s", res.Youtube))
@@ -350,7 +366,6 @@ func (pc *ProxyChecker) updateProxyName(res *Result, httpClient *ProxyClient, sp
 	}
 
 	res.Proxy["name"] = name
-
 }
 
 // showProgress 显示进度条